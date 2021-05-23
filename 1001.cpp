dbf87b8003741dae787be4cf0ec9ed25d2b366c3
Tanvirul Hasan <48523804+TanvirulHasan@users.noreply.github.com>
2021-05-24 02:31:13 +0600

    Add files via upload

---
+++ 1001.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    double r;
+    cin >> r >> endl;
+    double A = 3.14159*r*r;
+
+    cout << "A=" << A << endl;
+
+    return 0;
+
+}
